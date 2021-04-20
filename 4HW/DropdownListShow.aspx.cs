b8aa2e04d3e87cd4925eee4e47222d42aa3428c9
晉豪 劉 <107111236@mail.oit.edu.tw>
2021-04-20 22:21:11 +0800

    Homework 4

--- 4HW/DropdownListShow.aspx.cs
+++ 4HW/DropdownListShow.aspx.cs
@@ -9,6 +9,26 @@ namespace _4HW {
     public partial class DropdownListShow : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
+            if (dbl_Area.Items.Count==0)
+            {
+                dbl_Area.Items.Add("北區");
+                dbl_Area.Items.Add("中區");
+            }
+
+            if (dbl_Area.SelectedIndex == 0)
+            {
+                dbl_Place.Items.Clear();
+                dbl_Place.Items.Add("基隆");
+                dbl_Place.Items.Add("台北");
+                dbl_Place.Items.Add("新北");
+            }
+            else
+            {
+                dbl_Place.Items.Clear();
+                dbl_Place.Items.Add("苗栗");
+                dbl_Place.Items.Add("台中");
+                dbl_Place.Items.Add("南投");
+            }
         }
     }
 }
