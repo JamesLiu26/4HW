66a97561f3d626ad187278c8aaa6bf30d85924b5
晉豪 劉 <107111236@mail.oit.edu.tw>
2021-04-22 23:08:51 +0800

    Homework 4 finish

--- 4HW/DropdownListShow.aspx.cs
+++ 4HW/DropdownListShow.aspx.cs
@@ -9,26 +9,42 @@ namespace _4HW {
     public partial class DropdownListShow : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
 
+            string[] place = { "基隆", "台北", "新北", "苗栗", "台中", "南投" };
+
             if (dbl_Area.Items.Count==0)
             {
                 dbl_Area.Items.Add("北區");
                 dbl_Area.Items.Add("中區");
+                for (int p = 0; p < place.Length; p++)
+                {
+                    dbl_Place.Items.Add(place[p]);
+                    if (p < 3)
+                        dbl_Place.Items[p].Enabled = true;
+                    else
+                        dbl_Place.Items[p].Enabled = false;
+                }
             }
-
-            dbl_Place.Items.Clear();
 
             if (dbl_Area.SelectedIndex == 0)
             {
-                
-                dbl_Place.Items.Add("基隆");
-                dbl_Place.Items.Add("台北");
-                dbl_Place.Items.Add("新北");
+                for (int p = 0; p < place.Length; p++)
+                {
+                    if (p < 3)
+                        dbl_Place.Items[p].Enabled = true;
+                    else
+                        dbl_Place.Items[p].Enabled = false;
+                }
+
             }
             else
             {
-                dbl_Place.Items.Add("苗栗");
-                dbl_Place.Items.Add("台中");
-                dbl_Place.Items.Add("南投");
+                for (int p = 0; p < place.Length; p++)
+                {
+                    if (p > 2)
+                        dbl_Place.Items[p].Enabled = true;
+                    else
+                        dbl_Place.Items[p].Enabled = false;
+                }
             }
         }
     }
